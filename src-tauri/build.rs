a3bb9d5945afaf0c30e574e814f88e216bf52ca5
Deign86 <deigngreylazaro@plv.edu.ph>
2026-08-02 23:35:10 +0800

    Limit GUI linker flag to release builds

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,5 +1,7 @@
 fn main() {
-    println!("cargo:rustc-link-arg-bin=alpha-premier-attendance=/SUBSYSTEM:WINDOWS");
+    if std::env::var("PROFILE").as_deref() == Ok("release") {
+        println!("cargo:rustc-link-arg-bin=alpha-premier-attendance=/SUBSYSTEM:WINDOWS");
+    }
     tauri_build::build()
 }
 
